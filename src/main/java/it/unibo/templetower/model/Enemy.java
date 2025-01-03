113cd0fc6a257edc1f6ba166c7706e3ab9c23902
Nicolas Montanari <nicolas.montanari3@studio.unibo.it>
2025-01-03 09:47:26 +0100

    creation class PlayerImpl, add method attack Enemy

--- src/main/java/it/unibo/templetower/model/Enemy.java
+++ src/main/java/it/unibo/templetower/model/Enemy.java
@@ -1,5 +1,6 @@
 package it.unibo.templetower.model;
 
 public interface Enemy {
+    public void attack(Player player);
     void takeDamage(int damage);
 }
---
+++ src/main/java/it/unibo/templetower/model/PlayerImpl.java
@@ -0,0 +1,47 @@
+package it.unibo.templetower.model;
+
+public class PlayerImpl implements Player {
+
+    @Override
+    public void attack(Enemy enemy) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'attack'");
+    }
+
+    @Override
+    public void changeWeapon(Weapon weapon) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'changeWeapon'");
+    }
+
+    @Override
+    public void chooseMove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'chooseMove'");
+    }
+
+    @Override
+    public void moveForward() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveForward'");
+    }
+
+    @Override
+    public void moveBackward() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveBackward'");
+    }
+
+    @Override
+    public int getHealth() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHealth'");
+    }
+
+    @Override
+    public int getExperience() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getExperience'");
+    }
+    
+}
