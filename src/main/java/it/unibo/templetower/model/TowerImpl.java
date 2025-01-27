251c83dd1d37b218c4e501ab807be27e33254595
Nicolas Montanari <nicolas.montanari3@studio.unibo.it>
2025-01-27 23:11:01 +0100

    floor list in tower

---
+++ src/main/java/it/unibo/templetower/model/TowerImpl.java
@@ -0,0 +1,26 @@
+package it.unibo.templetower.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TowerImpl implements Tower{
+    private List<Floor> floor = new ArrayList<>();
+    @Override
+    public void generateLevels() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'generateLevels'");
+    }
+
+    @Override
+    public RoomBehavior getCurrentRoom() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCurrentRoom'");
+    }
+
+    @Override
+    public void movePlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'movePlayer'");
+    }
+    
+}
